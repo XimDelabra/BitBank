f3eafdbea8b0227d8191dd94b35a42b21f0def2a
Ximena Delabra <ximena.aledel@gmail.com>
2025-01-23 13:23:21 -0600

    Login y alertas

--- login.php
+++ login.php
@@ -1,5 +1,36 @@
 <?php
 require 'db_conexion.php';
+session_start();
+# Inicia Código de LOGIN
+if (isset($_POST['login'])) {
+    $cuenta_correo = $_POST['cuenta_correo'];
+    $password = $_POST['password'];;
+
+
+    if (!empty($cuenta_correo) && !empty($password)) {
+        $select = $cnnPDO->prepare('SELECT * from usuarios WHERE (correo = ? OR numero_cuenta = ?) AND password = ?');
+
+        $select->execute([$cuenta_correo, $cuenta_correo, $password]);
+        $count = $select->rowCount();
+        $campo = $select->fetch(PDO::FETCH_ASSOC);
+
+        if ($count) {
+            $_SESSION['numero_cuenta'] = $campo['numero_cuenta'];
+            $_SESSION['correo'] = $campo['correo'];
+            $_SESSION['nombre'] = $campo['nombre'];
+            $_SESSION['tipo_cuenta'] = $campo['tipo_cuenta'];
+            $_SESSION['password'] = $campo['password'];
+
+            header('location: mysesion.php');
+        } else {
+            $alertType = 'error';
+            $alertMessage = 'Mensaje: Credenciales Incorrectas.';
+        }
+    } else {
+        $alertType = 'error';
+        $alertMessage = 'Mensaje: Por favor, rellenar campos vacios.';
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,6 +58,43 @@ require 'db_conexion.php';
             </div>
         </div>
     </nav>
+    <div class="container">
+        <form method="POST" action="">
+            <div class="mb-3">
+                <label for="cuenta_correo" class="form-label ">Número de Cuenta / Correo</label>
+                <input type="text" class="form-control" id="cuenta_correo" name="cuenta_correo">
+            </div>
+
+            <div class="mb-3">
+                <label for="password" class="form-label">Contrasena</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+
+
+            <button type="submit" class="btn btn-primary" name="login">Iniciar Sesion</button>
+        </form>
+    </div>
+    <script>
+        const notyf = new Notyf();
+
+        function showNotification(type, message) {
+            if (type === 'success') {
+                notyf.success(message);
+            } else if (type === 'error') {
+                notyf.open({
+                    type: 'warning',
+                    message: message,
+                    duration: 5000, 
+                    background: 'orange', 
+                });
+            }
+        }
+
+        // Mostrar notificación si existe un mensaje desde PHP
+        <?php if (!empty($alertMessage)) : ?>
+            showNotification('<?= $alertType ?>', '<?= $alertMessage ?>');
+        <?php endif; ?>
+    </script>
 </body>
 
 </html>
--- registro.php
+++ registro.php
@@ -2,25 +2,82 @@
 require 'db_conexion.php';
 
 # Inicia Código de REGISTRAR
-
 if (isset($_POST['registrar'])) {
     $numero_cuenta = $_POST['numero_cuenta'];
     $nombre_cliente = $_POST['nombre_cliente'];
     $correo = $_POST['correo'];
     $tipo_cuenta = $_POST['tipo_cuenta'];
     $password = '';
-    
-    
+    $bytes = random_bytes(5);
+    $password = substr(bin2hex($bytes), 0, 5);
 
-    if (!empty($numero_cuenta) && !empty($nombre_cliente) && !empty($correo) && !empty($tipo_cuenta) ) {
-        $sql = $cnnPDO->prepare("INSERT INTO usuarios
-                (numero_cuenta,nombre,correo,tipo_cuenta,password) VALUES (?,?,?,?,?)");
-        $sql->execute([$_POST['numero_cuenta'], $_POST['nombre_cliente'], $_POST['correo'], $_POST['tipo_cuenta'], $password]);
 
-        $mensaje = "";
-       
+    if (!empty($numero_cuenta) && !empty($nombre_cliente) && !empty($correo) && !empty($tipo_cuenta)) {
+        try {
+            $sql = $cnnPDO->prepare("INSERT INTO usuarios
+                    (numero_cuenta,nombre,correo,tipo_cuenta,password) VALUES (?,?,?,?,?)");
+            $sql->execute([$numero_cuenta, $nombre_cliente, $correo, $tipo_cuenta, $password]);
+
+            $alertType = 'success';
+            $alertMessage = 'Registro exitoso. Se envió un correo con su contrasena.';
+
+            $mensaje = "<html>
+
+            <head>
+                <title>Correo desde la web</title>
+                
+            </head>
+
+            <body>
+                <table>
+                    <tr>
+                        <td>
+                            <div class='mail'>
+                                <h1>Correo enviado desde la web</h1><br>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <ol>
+                                <li>Tu numero de cuenta: <strong>$numero_cuenta</strong></li>
+                                <li>Tu nombre: <strong>$nombre_cliente</strong></li>
+                                <li>Tu correo: <strong>$correo</strong></li>
+                                <li>Tu tipo de cuenta: <strong>$tipo_cuenta</strong></li>
+                                <li>Tu nueva CONTRASENA: <strong>$password</strong></li>
+                            </ol>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOkKe7Eh7DaTREZ0Tz12-Kh7Ij0s9INOYP5g&s'>
+                        </td>
+                    </tr>
+                </table>
+            </body>
+
+            </html>";
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            $destinatario = $correo;
+            $asunto = "Correo enviado desde la web :)";
+
+            (mail($destinatario, $asunto, $mensaje, $headers));
+
+            unset($sql);
+            unset($cnnPDO);
+        } catch (PDOException $mensaje) {
+            if ($mensaje->getCode() == 23000) {
+                $alertType = 'error';
+                $alertMessage = 'Mensaje: El correo ya está registrado.';
+            } else {
+                $alertType = 'error';
+                $alertMessage = 'Mensaje: ' . $e->getMessage();
+            }
+        }
     } else {
-        
+        $alertType = 'error';
+        $alertMessage = 'Por favor, rellena todos los campos.';
     }
 }
 # Termina Código de REGISTRAR
@@ -32,6 +89,9 @@ if (isset($_POST['registrar'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/cleave.js/dist/cleave.min.js"></script>
+    
+
 </head>
 
 <body>
@@ -55,8 +115,8 @@ if (isset($_POST['registrar'])) {
     <div class="container">
         <form method="POST" action="">
             <div class="mb-3">
-                <label for="numero_cuenta" class="form-label">Número de Cuenta</label>
-                <input type="text" class="form-control" id="numero_cuenta" name="numero_cuenta">
+                <label for="numero_cuenta" class="form-label ">Número de Cuenta</label>
+                <input type="text" class="form-control input-number" id="numero_cuenta" name="numero_cuenta">
             </div>
             <div class="mb-3">
                 <label for="nombre_cliente" class="form-label">Nombre del cliente</label>
@@ -68,7 +128,7 @@ if (isset($_POST['registrar'])) {
             </div>
 
             <div class="mb-3">
-            <label for="tipo_cuenta" class="form-label">Tipo de Cuenta</label>
+                <label for="tipo_cuenta" class="form-label">Tipo de Cuenta</label>
                 <select class="form-select" id="tipo_cuenta" name="tipo_cuenta">
                     <option selected>Open this select menu</option>
                     <option value="debito">Debito</option>
@@ -79,6 +139,33 @@ if (isset($_POST['registrar'])) {
             <button type="submit" class="btn btn-primary" name="registrar">Registrar</button>
         </form>
     </div>
+    <script>
+        new Cleave('.input-number', {
+            blocks: [4, 4, 4, 4], // Cuatro bloques de 4 dígitos
+            numericOnly: true
+        });
+
+
+        const notyf = new Notyf();
+
+        function showNotification(type, message) {
+            if (type === 'success') {
+                notyf.success(message);
+            } else if (type === 'error') {
+                notyf.open({
+                    type: 'warning',
+                    message: message,
+                    duration: 5000, 
+                    background: 'orange', 
+                });
+            }
+        }
+
+        // Mostrar notificación si existe un mensaje desde PHP
+        <?php if (!empty($alertMessage)) : ?>
+        showNotification('<?= $alertType ?>', '<?= $alertMessage ?>');
+        <?php endif; ?>
+    </script>
 </body>
 
 </html>
