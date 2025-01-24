1d5e9eb9782bac975c8b8a00d38ffabd5cbe8f1b
Leonardo Nunez <leosternunez@gmail.com>
2025-01-24 00:32:40 -0600

    Front Correo Prueba 1

--- registro.php
+++ registro.php
@@ -38,44 +38,103 @@ if (isset($_POST['registrar'])) {
 
             $mensaje = "<html>
 
-            <head>
-                <title>Correo desde la web</title>
-                
-            </head>
+<head>
+    <!-- Poppins Font -->
+    <link rel='preconnect' href='https://fonts.googleapis.com'>
+    <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
+    <link
+        href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+        rel='stylesheet'>
+    <title>Registro Completado</title>
+    <style>
+        body {
+            background-image: url(images/bg-msg.jpg);
+            background-size: cover;
+            font-family: Poppins;
+        }
 
-            <body>
-                <table>
-                    <tr>
-                        <td>
-                            <div class='mail'>
-                                <h1>Correo enviado desde la web</h1><br>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <ol>
-                                <li>Tu numero de cuenta: <strong>$numero_cuenta</strong></li>
-                                <li>Tu nombre: <strong>$nombre_cliente</strong></li>
-                                <li>Tu correo: <strong>$correo</strong></li>
-                                <li>Tu tipo de cuenta: <strong>$tipo_cuenta</strong></li>
-                                <li>Tu nueva CONTRASENA: <strong>$password</strong></li>
-                            </ol>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOkKe7Eh7DaTREZ0Tz12-Kh7Ij0s9INOYP5g&s'>
-                        </td>
-                    </tr>
-                </table>
-            </body>
+        .table {
+            background-color: #c0c3c59f;
+            padding: 5px;
+           
+        }
 
-            </html>";
+        h1 {
+            font-weight: 200;
+            margin: 0;
+        }
+        .img-cont{
+            display: flex;
+            justify-content: end;
+        }
+        .img{
+            width: 60px;
+            
+        
+        }
+        .msg {
+            margin: 0;
+            font-size: 20px;
+        }
+
+        ul {
+            list-style: none;
+        }
+        .password{
+            text-align: center;
+            font-size: 35px;
+            font-weight: 200;
+            margin: 0;
+        }
+        .link{
+            text-align: center;
+        }
+    </style>
+
+</head>
+
+<body>
+    <table class='table'>
+        <tr class='img-cont'>
+            <td><img class='img' src='images/logo.png'></td>
+        </tr>
+        <tr>
+            <td>
+                <div class='mail'>
+                    <h1>Bienvenido $nombre_cliente</h1>
+                    <p class='msg'>Muchas gracias por confiar en nosotros</p><br>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p>Tus datos son los siguientes:</p>
+                <ul>
+                    <li>Numero de cuenta: <strong>$numero_cuenta</strong></li>
+                    <li>Nombre: <strong>$nombre_cliente</strong></li>
+                    <li>Correo: <strong>$correo</strong></li>
+                    <li>Tipo de cuenta: <strong>$tipo_cuenta</strong></li>
+                </ul>
+            </td>
+        <tr>
+            <td>
+                <p class='msg'>Podras acceder a tu cuenta con la contrasena:</p>
+                <p class='password'>$password</p>
+                <p class='link'>En la siguiente liga <a href='login.php'>Iniciar Sesion</a></p>
+            </td>
+        </tr>
+        </tr>
+        <tr>
+
+        </tr>
+    </table>
+</body>
+
+</html>";
             $headers = "MIME-Version: 1.0" . "\r\n";
             $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
             $destinatario = $correo;
-            $asunto = "Correo enviado desde la web :)";
+            $asunto = "Registro Completo | BitBank";
 
             (mail($destinatario, $asunto, $mensaje, $headers));
 
