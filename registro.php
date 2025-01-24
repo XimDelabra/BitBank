f5063ef4ac95dae195dbb6d19ec380f9714e6193
Leonardo Nunez <leosternunez@gmail.com>
2025-01-23 22:59:01 -0600

    Front Registro

--- registro.php
+++ registro.php
@@ -13,24 +13,22 @@ if (isset($_POST['registrar'])) {
 
     if (!empty($numero_cuenta) && !empty($nombre_cliente) && !empty($correo) && !empty($tipo_cuenta)) {
         #Verificamos que el correo no este registrado
-            $select = $cnnPDO->prepare("SELECT * FROM usuarios WHERE correo = ?");
-            $select->execute([$correo]);
-            $correo_registrado = $select->fetchColumn();
+        $select = $cnnPDO->prepare("SELECT * FROM usuarios WHERE correo = ?");
+        $select->execute([$correo]);
+        $correo_registrado = $select->fetchColumn();
 
         #Verificamos que el numero de cuenta no este registrado
-            $select_number = $cnnPDO->prepare("SELECT * FROM usuarios WHERE numero_cuenta = ?");
-            $select_number->execute([$numero_cuenta]);
-            $numero_registrado = $select_number->fetchColumn();
+        $select_number = $cnnPDO->prepare("SELECT * FROM usuarios WHERE numero_cuenta = ?");
+        $select_number->execute([$numero_cuenta]);
+        $numero_registrado = $select_number->fetchColumn();
 
         if ($correo_registrado > 0) {
             $alertType = 'error';
             $alertMessage = 'Mensaje: El correo ' . $correo . ' ya está registrado.';
-
-        } elseif ($numero_registrado > 0){
+        } elseif ($numero_registrado > 0) {
             $alertType = 'error';
             $alertMessage = 'Mensaje: El numero de cuenta ' . $numero_cuenta . ' ya está registrado.';
-
-        } else{
+        } else {
             $sql = $cnnPDO->prepare("INSERT INTO usuarios
                     (numero_cuenta,nombre,correo,tipo_cuenta,password) VALUES (?,?,?,?,?)");
             $sql->execute([$numero_cuenta, $nombre_cliente, $correo, $tipo_cuenta, $password]);
@@ -83,7 +81,6 @@ if (isset($_POST['registrar'])) {
 
             unset($sql);
             unset($cnnPDO);
-        
         }
     } else {
         $alertType = 'success';
@@ -98,9 +95,15 @@ if (isset($_POST['registrar'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>BitBank | Registro</title>
+    <!-- Poppins Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
     <script src="https://cdn.jsdelivr.net/npm/cleave.js/dist/cleave.min.js"></script>
-   
+    <link rel="stylesheet" href="style.css">
+
     <style>
         .notyf__icon i {
             color: white !important;
@@ -114,50 +117,56 @@ if (isset($_POST['registrar'])) {
 
 </head>
 
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+<body class="registro-body">
+    <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand text-light" href="index.php">
+                <img class="logo" src="images/logo.png" alt="">
+                BitBank
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                    <a class="nav-link" href="registro.php">Registrar</a>
-                    <a class="nav-link" href="login.php">Inicia Sesion</a>
+                    <a class="nav-link text-light" href="registro.php">Registrar</a>
+                    <a class="nav-link text-light" href="login.php">Inicia Sesion</a>
 
                 </div>
             </div>
         </div>
     </nav>
+    <div class="container position-relative">
+        <div class="container mt-3 mb-5 py-4 px-5 registro-form position-absolute top-50 start-50">
+            <form method="POST" action="">
+                <div class="mb-3">
+                    <label for="numero_cuenta" class="form-label ">Número de Cuenta</label>
+                    <input type="text" class="form-control input-number" id="numero_cuenta" name="numero_cuenta">
+                </div>
+                <div class="mb-3">
+                    <label for="nombre_cliente" class="form-label">Nombre del cliente</label>
+                    <input type="text" class="form-control" id="nombre_cliente" name="nombre_cliente">
+                </div>
+                <div class="mb-3">
+                    <label for="correo" class="form-label">Correo</label>
+                    <input type="email" class="form-control" id="correo" name="correo">
+                </div>
 
-    <div class="container">
-        <form method="POST" action="">
-            <div class="mb-3">
-                <label for="numero_cuenta" class="form-label ">Número de Cuenta</label>
-                <input type="text" class="form-control input-number" id="numero_cuenta" name="numero_cuenta">
-            </div>
-            <div class="mb-3">
-                <label for="nombre_cliente" class="form-label">Nombre del cliente</label>
-                <input type="text" class="form-control" id="nombre_cliente" name="nombre_cliente">
-            </div>
-            <div class="mb-3">
-                <label for="correo" class="form-label">Correo</label>
-                <input type="email" class="form-control" id="correo" name="correo">
-            </div>
+                <div class="mb-3">
+                    <label for="tipo_cuenta" class="form-label">Tipo de Cuenta</label>
+                    <select class="form-select mb-4" id="tipo_cuenta" name="tipo_cuenta">
+                        <option selected>Elija una opcion...</option>
+                        <option value="debito">Debito</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="credito">Tarjeta de Credito</option>
+                    </select>
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-light" style="width: 100%;" name="registrar">Registrar</button>
+                </div>
 
-            <div class="mb-3">
-                <label for="tipo_cuenta" class="form-label">Tipo de Cuenta</label>
-                <select class="form-select" id="tipo_cuenta" name="tipo_cuenta">
-                    <option selected>Open this select menu</option>
-                    <option value="debito">Debito</option>
-                    <option value="ahorro">Ahorro</option>
-                    <option value="credito">Tarjeta de Credito</option>
-                </select>
-            </div>
-            <button type="submit" class="btn btn-primary" name="registrar">Registrar</button>
-        </form>
+            </form>
+        </div>
     </div>
     <script>
         new Cleave('.input-number', {
